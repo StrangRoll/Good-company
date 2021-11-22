b16f660508318fe1011b6186e57173312e2a1498
StrangRoll <strrranger1@gmail.com>
2021-11-22 23:05:43 +0300

    Fixed some bug

--- Assets/Scripts/DropPlace.cs
+++ Assets/Scripts/DropPlace.cs
@@ -5,11 +5,12 @@ using UnityEngine.EventSystems;
 
 public class DropPlace : MonoBehaviour, IDropHandler
 {
+    [SerializeField] int maxCountCards;
     public void OnDrop(PointerEventData eventData)
     {
         Card card = eventData.pointerDrag.GetComponent<Card>();
 
-        if (card)
+        if (card && transform.childCount < maxCountCards)
         {
             card.defaultParent = transform;
         }
