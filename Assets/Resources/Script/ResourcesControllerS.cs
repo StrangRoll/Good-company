85be65fb182f136cf7c1c5c696d74184ae4fe3a9
StrangRoll <strrranger1@gmail.com>
2021-11-23 03:14:07 +0300

    Added resources and script to change them

---
+++ Assets/Resources/Script/ResourcesControllerS.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResourcesControllerS : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI people, trust, money, evilScore;
+
+    public void ChangePeople(int count)
+    {
+        int newCount;
+        int.TryParse(people.text, out newCount);
+        newCount += count;
+        people.text = newCount.ToString();
+    }
+
+    public void ChangeTrust(int count)
+    {
+        int newCount;
+        int.TryParse(trust.text, out newCount);
+        newCount += count;
+        trust.text = newCount.ToString();
+    }
+
+    public void ChangeMoney(int count)
+    {
+        int newCount;
+        int.TryParse(money.text, out newCount);
+        newCount += count;
+        money.text = newCount.ToString();
+    }
+
+    public void ChangeEvilScore(int count)
+    {
+        int newCount;
+        int.TryParse(evilScore.text, out newCount);
+        newCount += count;
+        evilScore.text = newCount.ToString();
+    }
+
+}
